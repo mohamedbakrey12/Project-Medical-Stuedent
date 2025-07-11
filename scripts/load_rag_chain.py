9696fed7504a546b3413aefa88d08bfa536c9888
Mohamed Bakrey <66233001+mohamedbakrey12@users.noreply.github.com>
2025-07-12 02:07:04 +0300

    Update load_rag_chain.py

--- scripts/load_rag_chain.py
+++ scripts/load_rag_chain.py
@@ -7,6 +7,8 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+
 
 
 
